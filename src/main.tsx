4e84e5871dc52293b186d8b71a191ec3d8314e24
Esteban Medrano <victor.medrano.z@ucb.edu.bo>
2026-07-28 22:30:14 -0400

    fix: unregister stale service workers from v1

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { registerServiceWorker } from './registerSW';
 
+// Desregistra cualquier service worker viejo (de la v1 o versiones anteriores)
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(registrations => {
+    for (const reg of registrations) {
+      reg.unregister();
+    }
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
